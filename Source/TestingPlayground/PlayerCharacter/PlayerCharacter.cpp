b169376bca63e4193cb33442a9ddf19e8853dae4
OskiC <xoskarx1@gmail.com>
2026-03-24 22:33:35 +0100

    create PlayerCharacter

---
+++ Source/TestingPlayground/PlayerCharacter/PlayerCharacter.cpp
@@ -0,0 +1,85 @@
+﻿#include "PlayerCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+APlayerCharacter::APlayerCharacter()
+{
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.f);
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+	MovementComponent = CreateDefaultSubobject<UCharacterMovementComponent>(TEXT("MovementComponent"));
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	CameraComponent->SetupAttachment(GetCapsuleComponent());
+	CameraComponent->SetRelativeLocation(FVector(-10.f, 0.f, 60.f));
+	CameraComponent->bUsePawnControlRotation = true;
+	
+	SkeletalMeshComponent->SetOnlyOwnerSee(true);
+	SkeletalMeshComponent->SetupAttachment(CameraComponent);
+	SkeletalMeshComponent->bCastDynamicShadow = true;
+	SkeletalMeshComponent->CastShadow = true;
+	SkeletalMeshComponent->SetRelativeLocation(FVector(-30.f, 0.f, -150.f));
+}
+
+void APlayerCharacter::NotifyControllerChanged()
+{
+	Super::NotifyControllerChanged();
+	
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("'%s' Failed to find an Enhanced Input Component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+	}
+}
+
+void APlayerCharacter::Look(const struct FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+	
+	if (IsValid(Controller))
+	{
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void APlayerCharacter::Move(const struct FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+	
+	if (IsValid(Controller))
+	{
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
+}
+
+FVector APlayerCharacter::GetInteractionStart() const {
+	return CameraComponent ? CameraComponent->GetComponentLocation() : FVector::ZeroVector;
+}
+
+FVector APlayerCharacter::GetInteractionDirection() const {
+	return CameraComponent ? CameraComponent->GetForwardVector() : FVector::ForwardVector;
+}
---
+++ Source/TestingPlayground/PlayerCharacter/PlayerCharacter.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include "GameFramework/Character.h"
+
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+	
+public:
+	APlayerCharacter();
+	
+	FVector GetInteractionStart() const;
+	FVector GetInteractionDirection() const;
+
+protected:
+	void Move(const struct FInputActionValue& Value);
+	void Look(const struct FInputActionValue& Value);
+	virtual void NotifyControllerChanged() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent = nullptr;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UCharacterMovementComponent> MovementComponent = nullptr;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UCameraComponent> CameraComponent = nullptr;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UInputMappingContext> DefaultMappingContext = nullptr;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UInputAction> JumpAction = nullptr;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UInputAction> MoveAction = nullptr;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UInputAction> LookAction = nullptr;
+};
