d1dba5121d58da9ef4148dae077ede2e5b0cc097
OskiC <xoskarx1@gmail.com>
2026-03-25 19:19:17 +0100

    Create CustomPlayerState which contains AbilitySystemComponent

---
+++ Source/TestingPlayground/PlayerState/CustomPlayerState.cpp
@@ -0,0 +1,27 @@
+﻿#include "CustomPlayerState.h"
+
+#include "AbilitySystemComponent.h"
+#include "TestingPlayground/Abilities/CustomAbilitySystemComponent.h"
+
+ACustomPlayerState::ACustomPlayerState()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UCustomAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Full);
+}
+
+UAbilitySystemComponent* ACustomPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;	
+}
+
+void ACustomPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (IsValid(AbilitySystemComponent))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerState i AbilitySystemComp gotowe?"));
+	}
+}
---
+++ Source/TestingPlayground/PlayerState/CustomPlayerState.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
+
+#include "CustomPlayerState.generated.h"
+
+UCLASS()
+class ACustomPlayerState : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+	
+public:
+	ACustomPlayerState();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
+protected:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UCustomAbilitySystemComponent> AbilitySystemComponent = nullptr;
+	
+	virtual void BeginPlay() override;
+};
