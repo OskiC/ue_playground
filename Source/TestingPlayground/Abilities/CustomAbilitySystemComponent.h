c827579836fdcce59314d0b6747572dfbece5eac
OskiC <xoskarx1@gmail.com>
2026-03-25 19:18:59 +0100

    Create CustomAbilitySystemComponent

---
+++ Source/TestingPlayground/Abilities/CustomAbilitySystemComponent.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "AbilitySystemComponent.h"
+
+#include "CustomAbilitySystemComponent.generated.h"
+
+UCLASS()
+class UCustomAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
