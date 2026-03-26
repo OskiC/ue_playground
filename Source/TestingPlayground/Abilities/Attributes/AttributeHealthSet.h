fd45324d3ed35392d4726c07bee51e1c7e5ebc66
OskiC <xoskarx1@gmail.com>
2026-03-26 18:33:22 +0100

    Create AttributeHealthSet with Health and MaxHealth attribute data

---
+++ Source/TestingPlayground/Abilities/Attributes/AttributeHealthSet.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "AttributeSet.h"
+
+#include "AttributeHealthSet.generated.h"
+
+UCLASS()
+class UAttributeHealthSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FGameplayAttributeData Health;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FGameplayAttributeData MaxHealth;
+};
