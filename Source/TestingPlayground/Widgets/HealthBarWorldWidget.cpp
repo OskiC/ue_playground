f7e3716c1b4d12b39ad77d490dbd6f9d39a142f2
OskiC <xoskarx1@gmail.com>
2026-04-17 00:36:47 +0200

    add HealthBarWorldWidget

---
+++ Source/TestingPlayground/Widgets/HealthBarWorldWidget.cpp
@@ -0,0 +1,39 @@
+﻿#include "HealthBarWorldWidget.h"
+
+#include "Components/TextBlock.h"
+#include "Components/ProgressBar.h"
+
+#include "TestingPlayground/Abilities/Attributes/AttributeHealthSet.h"
+#include "TestingPlayground/PlayerState/CustomPlayerState.h"
+
+void UHealthBarWorldWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	ACustomPlayerState* PS = Cast<ACustomPlayerState>(GetOwningPlayerState());
+	if (IsValid(PS))
+	{
+		UAttributeHealthSet* AttributeHealthSet =  PS->GetAttributeHealthSet();
+		if (IsValid(AttributeHealthSet))
+		{
+			AttributeHealthSet->OnHealthChanged.AddDynamic(this, &UHealthBarWorldWidget::OnHealthUpdated);
+		
+			OnHealthUpdated(AttributeHealthSet->GetHealth(), AttributeHealthSet->GetHealth(), AttributeHealthSet->GetMaxHealth());
+		}
+	}
+	
+}
+
+void UHealthBarWorldWidget::OnHealthUpdated(float OldValue, float NewValue, float MaxHealth)
+{
+	FString HealthText = FString::Printf(TEXT("%.2f / %.2f"),
+		NewValue,
+		MaxHealth
+	);
+	
+	HealthLiteral->SetText(FText::FromString(HealthText));
+	HealthBar->SetPercent(NewValue / MaxHealth);
+	
+	// [TODO] : Add calculation where we will display damage
+	// Thinking: this only works for CustomPlayerState so there will be needed change of abstractions
+}
---
+++ Source/TestingPlayground/Widgets/HealthBarWorldWidget.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "Blueprint/UserWidget.h"
+
+#include "HealthBarWorldWidget.generated.h"
+
+UCLASS()
+class UHealthBarWorldWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* HealthLiteral;
+	
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* HealthBar;
+
+private:
+	UFUNCTION()
+	void OnHealthUpdated(float OldValue, float NewValue, float MaxHealth);
+};
