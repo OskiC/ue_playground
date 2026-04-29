eae0c71a01baab0695f23d6d86b44e6a732f9af5
OskiC <xoskarx1@gmail.com>
2026-04-29 14:59:58 +0200

    create UPlayerHUDWidget

---
+++ Source/TestingPlayground/Widgets/PlayerWidgets/PlayerHUDWidget.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Blueprint/UserWidget.h"
+
+#include "PlayerHUDWidget.generated.h"
+
+UCLASS()
+class UPlayerHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	void ShowTooltip(const FText& ToolTipText);
+	
+	void HideTooltip();
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class FTextBlock> TooltipText;
+};
