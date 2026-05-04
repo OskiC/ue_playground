0725b39a86df8f66b3fcee804e0898c6ecc20603
OskiC <xoskarx1@gmail.com>
2026-05-04 18:47:16 +0200

    create base hud with default widget

---
+++ Source/TestingPlayground/HUD/CustomHUD.cpp
@@ -0,0 +1,17 @@
+﻿#include "CustomHUD.h"
+
+#include "Blueprint/UserWidget.h"
+#include "TestingPlayground/Widgets/PlayerWidgets/PlayerHUDWidget.h"
+
+void ACustomHUD::BeginPlay()
+{
+	if (PlayerHUDWidgetClass)
+	{
+		PlayerHUDWidget = CreateWidget<UPlayerHUDWidget>(GetWorld(), PlayerHUDWidgetClass);
+		if (IsValid(PlayerHUDWidget))
+		{
+			PlayerHUDWidget->AddToViewport();
+			PlayerHUDWidget->HideTooltip();
+		}
+	}
+}
---
+++ Source/TestingPlayground/HUD/CustomHUD.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "GameFramework/HUD.h"
+
+#include "CustomHUD.generated.h"
+
+UCLASS()
+class ACustomHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Widgets")
+	TSubclassOf<class UPlayerHUDWidget> PlayerHUDWidgetClass;
+	
+	UPROPERTY()
+	TObjectPtr<class UPlayerHUDWidget> PlayerHUDWidget = nullptr;
+};
