025bb153bc686d2498cb3247b7db5731e1e2365f
OskiC <xoskarx1@gmail.com>
2026-05-06 19:22:06 +0200

    add InteractableInterface

---
+++ Source/TestingPlayground/Interfaces/InteractableInterface.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "InteractableInterface.generated.h"
+
+UINTERFACE()
+class UInteractableInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class IInteractableInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnHoverBegin() = 0;
+	virtual void OnHoverEnd() = 0;
+	virtual void OnInteract(APawn* Instigator) = 0;
+	virtual FText GetInteractionTooltip() const = 0;
+};
