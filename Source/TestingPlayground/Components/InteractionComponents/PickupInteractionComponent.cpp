f2df9a8e69b9b80cd9fcbf76e20b0a5b921572a9
OskiC <xoskarx1@gmail.com>
2026-05-06 19:24:03 +0200

    create PickupInteractionComponent class

---
+++ Source/TestingPlayground/Components/InteractionComponents/PickupInteractionComponent.cpp
@@ -0,0 +1,1 @@
+﻿#include "PickupInteractionComponent.h"
---
+++ Source/TestingPlayground/Components/InteractionComponents/PickupInteractionComponent.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "InteractionComponentBase.h"
+
+#include "PickupInteractionComponent.generated.h"
+
+UCLASS(ClassGroup=(PLAYGROUND), meta=(BlueprintSpawnableComponent))
+class UPickupInteractionComponent : public UInteractionComponentBase
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
