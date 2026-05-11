ee2a44b5323fcb42f8b04acc6614e3e3ce59543e
OskiC <xoskarx1@gmail.com>
2026-05-11 21:21:58 +0200

    add basic Utils

---
+++ Source/TestingPlayground/Utils/TestingPlaygroundUtils.cpp
@@ -0,0 +1,18 @@
+﻿#include "TestingPlaygroundUtils.h"
+
+#include "GameplayEffect.h"
+
+bool UTestingPlaygroundUtils::TestingPlayground_IsDurationGESpec(FGameplayEffectSpecHandle Handle)
+{
+	if (Handle.Data && Handle.Data->Def)
+	{
+		return Handle.Data->Def->DurationPolicy != EGameplayEffectDurationType::Instant;
+	}
+	
+	return false;
+}
+
+// bool UTestingPlaygroundUtils::TestingPlayground_IsValidActiveGESpec(FActiveGameplayEffectHandle Handle)
+// {
+// 	
+// }
---
+++ Source/TestingPlayground/Utils/TestingPlaygroundUtils.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "GameplayEffectTypes.h"
+
+#include "TestingPlaygroundUtils.generated.h"
+
+UCLASS()
+class UTestingPlaygroundUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "OwnUtils")
+	static bool TestingPlayground_IsDurationGESpec(FGameplayEffectSpecHandle Handle);
+	
+	// UFUNCTION(BlueprintCallable, Category = "OwnUtils")
+	// static bool TestingPlayground_IsValidActiveGESpec(FActiveGameplayEffectHandle Handle);
+};
