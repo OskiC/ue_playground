30e7d4c6937f6aed00747b26500017da90d82b35
OskiC <xoskarx1@gmail.com>
2026-05-14 19:48:12 +0200

    add proper validation

--- Source/TestingPlayground/Abilities/GameplayAbilities/UGA_Interact.cpp
+++ Source/TestingPlayground/Abilities/GameplayAbilities/UGA_Interact.cpp
@@ -15,10 +15,35 @@ void UGA_Interact::ActivateAbility(const FGameplayAbilitySpecHandle Handle, cons
 	if (TriggerEventData && TriggerEventData->OptionalObject)
 	{
 		UObject* TargetObj = const_cast<UObject*>(TriggerEventData->OptionalObject.Get());
+		AActor* TargetActor = Cast<AActor>(TargetObj);
 
-		if (IInteractableInterface* InteractableTarget = Cast<IInteractableInterface>(TargetObj))
+		if (!TargetActor)
 		{
-			InteractableTarget->OnInteract(Cast<APawn>(ActorInfo->AvatarActor.Get()));
+			if (UActorComponent* TargetComp = Cast<UActorComponent>(TargetObj))
+			{
+				TargetActor = TargetComp->GetOwner();
+			}
+		}
+
+		if (TargetActor && ActorInfo->AvatarActor.IsValid())
+		{
+			if (ActorInfo->IsNetAuthority())
+			{
+				const float MaxInteractDistance = 350.f;
+				const float Distance = ActorInfo->AvatarActor->GetDistanceTo(TargetActor);
+
+				if (Distance > MaxInteractDistance)
+				{
+					UE_LOG(LogTemp, Warning, TEXT("Interaction failed: Target too far away (Distance: %f)"), Distance);
+					EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+					return;
+				}
+			}
+
+			if (IInteractableInterface* InteractableTarget = Cast<IInteractableInterface>(TargetObj))
+			{
+				InteractableTarget->OnInteract(Cast<APawn>(ActorInfo->AvatarActor.Get()));
+			}
 		}
 	}
 
