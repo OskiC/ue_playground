f005903ebf809559262a70bfb1a6b532616883ee
OskiC <xoskarx1@gmail.com>
2026-07-27 21:51:53 +0200

    Add delegate, add getters, adjust AddToInventory

--- Source/TestingPlayground/Components/InventoryComponents/InventoryComponent.cpp
+++ Source/TestingPlayground/Components/InventoryComponents/InventoryComponent.cpp
@@ -1,9 +1,22 @@
 #include "InventoryComponent.h"
+
 #include <Net/UnrealNetwork.h>
+#include "Engine/ActorChannel.h"
 
 UInventoryComponent::UInventoryComponent()
 {
+	SetIsReplicatedByDefault(true);
 
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UInventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ActiveInventory.Empty();
+	ActiveInventory.SetNum(InventoryCapacity);
+	// in the future add some starting items
 }
 
 void UInventoryComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
@@ -13,23 +26,101 @@ void UInventoryComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& 
 	DOREPLIFETIME(UInventoryComponent, ActiveInventory);
 }
 
-bool UInventoryComponent::AddToInventory(FInventoryItemSlot ItemToAdd)
+bool UInventoryComponent::ReplicateSubobjects(UActorChannel* Channel, FOutBunch* Bunch, FReplicationFlags* RepFlags)
 {
-	UItemDefinition* ItemType = ItemToAdd.ItemDefinition;
-	for (auto& Item : ActiveInventory)
+	bool bWroteSomething = Super::ReplicateSubobjects(Channel, Bunch, RepFlags);
+	for (const FInventoryItemSlot& Slot : ActiveInventory)
 	{
-		if (Item.ItemDefinition == ItemType)
+		if (Slot.IsStructValid())
 		{
-			Item.StackCount += ItemToAdd.StackCount;
-			return true;
+			bWroteSomething |= Channel->ReplicateSubobject(Slot.ItemInstance, *Bunch, *RepFlags);
 		}
 	}
+	return bWroteSomething;
+}
 
-	if (ActiveInventory.Num() >= InventoryCapacity)
+bool UInventoryComponent::AddToInventory(FInventoryItemSlot ItemToAdd)
+{
+	if (!GetOwner()->HasAuthority())
 	{
 		return false;
 	}
 
-	ActiveInventory.Emplace(ItemToAdd);
-	return true;
+	if (!ItemToAdd.IsStructValid() || ItemToAdd.StackCount <= 0)
+	{
+		return false;
+	}
+
+	int32 AmountRemaining = ItemToAdd.StackCount;
+	int32 MaxStack = ItemToAdd.ItemInstance->ItemDef->MaxStackSize;
+	bool bInventoryChanged = false;
+
+	for (FInventoryItemSlot& ExistingSlot : ActiveInventory)
+	{
+		if (ExistingSlot.IsStructValid() && ExistingSlot.ItemInstance->ItemDef == ItemToAdd.ItemInstance->ItemDef)
+		{
+			int32 RoomLeftInStack = MaxStack - ExistingSlot.StackCount;
+			if (RoomLeftInStack > 0)
+			{
+				int32 AmountToTake = FMath::Min(AmountRemaining, RoomLeftInStack);
+				ExistingSlot.StackCount += AmountToTake;
+				AmountRemaining -= AmountToTake;
+				bInventoryChanged = true;
+
+				if (AmountRemaining <= 0)
+				{
+					break;
+				}
+			}
+		}
+	}
+
+	while (AmountRemaining > 0)
+	{
+		int32 EmptyIndex = INDEX_NONE;
+
+		for (int32 i = 0; i < ActiveInventory.Num(); i++)
+		{
+			if (!ActiveInventory[i].IsStructValid())
+			{
+				EmptyIndex = i;
+				break;
+			}
+		}
+
+		if (EmptyIndex == INDEX_NONE)
+		{
+			break;
+		}
+
+		int32 AmountForNewSlot = FMath::Min(AmountRemaining, MaxStack);
+
+		ActiveInventory[EmptyIndex].ItemInstance = ItemToAdd.ItemInstance;
+		ActiveInventory[EmptyIndex].StackCount = AmountForNewSlot;
+
+		AmountRemaining -= AmountForNewSlot;
+		bInventoryChanged = true;
+	}
+
+	if (bInventoryChanged)
+	{
+		OnInventoryUpdated.Broadcast();
+	}
+
+	return AmountRemaining <= 0;
+}
+
+TArray<FInventoryItemSlot> UInventoryComponent::GetActiveInventory() const
+{
+	return ActiveInventory;
+}
+
+int32 UInventoryComponent::GetInventoryCapacity() const
+{
+	return InventoryCapacity;
+}
+
+void UInventoryComponent::OnRep_ActiveInventory()
+{
+	OnInventoryUpdated.Broadcast();
 }
--- Source/TestingPlayground/Components/InventoryComponents/InventoryComponent.h
+++ Source/TestingPlayground/Components/InventoryComponents/InventoryComponent.h
@@ -4,6 +4,8 @@
 
 #include "InventoryComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnInventoryUpdated);
+
 UCLASS()
 class UInventoryComponent : public UActorComponent
 {
@@ -12,18 +14,30 @@ class UInventoryComponent : public UActorComponent
 public:
 	UInventoryComponent();
 
+	virtual void BeginPlay() override;
+
 	bool AddToInventory(FInventoryItemSlot ItemToAdd);
 	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	int32 GetInventoryCapacity() const;
+	TArray<FInventoryItemSlot> GetActiveInventory() const;
+
+	virtual bool ReplicateSubobjects(class UActorChannel* Channel, FOutBunch* Bunch, FReplicationFlags* RepFlags);
+
+	UPROPERTY(BlueprintAssignable, Category = "Inventory")
+	FOnInventoryUpdated OnInventoryUpdated;
 
 protected:
 	UPROPERTY(EditAnywhere, Category = "Inventory")
 	TArray<FStartingItemDef> StartingItems;
 
-	UPROPERTY(Replicated)
+	UPROPERTY(ReplicatedUsing = OnRep_ActiveInventory)
 	TArray<FInventoryItemSlot> ActiveInventory;
 
 	UPROPERTY(EditAnywhere, Category = "Inventory")
 	int32 InventoryCapacity = 0;
+
+	UFUNCTION()
+	void OnRep_ActiveInventory();
 
 private:
 
